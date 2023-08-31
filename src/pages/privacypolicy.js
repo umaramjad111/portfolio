0637d545d2d3f6244f4fcb0f1d354de1ce01a2d9
umaramjad <umaramjad489@gmail.com>
2023-09-01 02:26:22 +0500

    privacy

--- src/pages/privacypolicy.js
+++ src/pages/privacypolicy.js
@@ -11,10 +11,19 @@ export default function Privacypolicy() {
         <p>If you choose to get in touch with me via the contact form or email provided on my website, I will collect the personal information you provide, such as your name and email address. I will use this information solely to respond to your inquiries, provide information about my services, or engage in professional communication.</p>
         <h4 className='mt-3'>Cookies and Tracking</h4>
         <p>My website may use cookies, which are small text files stored on your device to enhance your browsing experience. Cookies allow me to remember your preferences and provide personalized content.</p>
+        <h4 className='mt-3'>GDPR Compliance</h4>
+        <p>We are committedto complying with General Data Protection Regulation (GDPR) when handling personal data, Our website designed
+         to protect the privacy of EU citizens and we collect,process, and store personal data in accordance in GDPR requirements.
+        </p>
         <h4 className='mt-3'>Data Security</h4>
         <p>I take data security seriously and implement reasonable measures to protect the information you provide. However, please be aware that no online platform can guarantee complete security, and you should take precautions when sharing personal information online.</p>
         <h4 className='mt-3'>Your Rights</h4>
         <p>As a visitor to my website, you have the right to access, update, or delete the personal information I hold about you. You can also choose to restrict or object to the processing of your data. If you have any concerns about how your data is being used, please feel free to contact me using the provided contact information.</p>
+        <h4>CCPA Compliance</h4>
+        <p>We are also committed to complying with California Consumer Privacy Act (CCPA) when handling 
+            personal data of California residents. Our website is designed to protect privacy of California
+            residents and we collect, process, and store personal data in accordance with CCPA requirements.
+        </p>
         <h4 className='mt-3'>Policy Updates</h4>
         <p>I may update this privacy policy from time to time to reflect changes in my practices or to comply with legal requirements. The latest version of the privacy policy will be posted on this page, along with the date of the last update.</p>
     </div>
