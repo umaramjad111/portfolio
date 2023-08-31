bfc2070b1f2e50d26fdb8a36358189393cd6d7f3
umaramjad <umaramjad489@gmail.com>
2023-08-31 10:41:08 +0500

    set

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav class="navbar navbar-expand-lg  main_nav">
     <div class="container-fluid">
-      <a class="navbar-brand mainHeading" href="#"> <span>U</span>MAR</a>
+      <a class="navbar-brand mainHeading" href="/"> <span>U</span>MAR</a>
       
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -13,19 +13,19 @@ export default function Navbar() {
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">HOME</a>
+            <a class="nav-link" aria-current="page" href="/">HOME</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">ABOUT ME</a>
+            <a class="nav-link" href="/aboutus">ABOUT ME</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">PROJECTS</a>
+            <a class="nav-link" href="/projects">PROJECTS</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">SERVICES</a>
+            <a class="nav-link" href="/services">SERVICES</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">CONTACT</a>
+            <a class="nav-link" href="/contact">CONTACT</a>
           </li>
           <button className='contact_btn'>GET INTOUCH ME</button>
         </ul>
